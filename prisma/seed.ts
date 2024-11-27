ba0849671d9fa503151e2ec03d93189b27024183
Abanoub Nagy <abanoubnagy60@gmail.com>
2024-11-27 17:23:21 +0200

    error handling

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,26 +4,17 @@ import { faker } from '@faker-js/faker';
 const prisma = new PrismaClient()
 
 async function main() {
-  // Users
-  // const user = await prisma.user.createMany({
-  //   data: Array.from({ length: 20 }, () => {
-  //     return {
-  //       email: faker.internet.email(),
-  //       name: faker.internet.username(),
-  //     }
-  //   })
-  // })
 
-  // todos
+  // todos ===>> you can delete this step 
   const todo=await prisma.todo.createMany({
     data:Array.from({length:10},()=>{
       return {
         title:faker.lorem.word(),
-        body:faker.lorem.paragraph()
+        body:faker.lorem.paragraph(),
+        user_id:""
       }
     })
   }) 
-
 
   console.log('Seeded database with test user:', todo)
 }

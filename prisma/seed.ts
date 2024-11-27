01aebae44ea815ebba07522429c72ddd072c5bde
Abanoub Nagy <abanoubnagy60@gmail.com>
2024-11-27 18:04:43 +0200

    fix deployment errors

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,22 +1,22 @@
 import { PrismaClient } from '@prisma/client'
-import { faker } from '@faker-js/faker';
+// import { faker } from '@faker-js/faker';
 
 const prisma = new PrismaClient()
 
 async function main() {
 
   // todos ===>> you can delete this step 
-  const todo=await prisma.todo.createMany({
-    data:Array.from({length:10},()=>{
-      return {
-        title:faker.lorem.word(),
-        body:faker.lorem.paragraph(),
-        user_id:""
-      }
-    })
-  }) 
+  // const todo=await prisma.todo.createMany({
+  //   data:Array.from({length:10},()=>{
+  //     return {
+  //       title:faker.lorem.word(),
+  //       body:faker.lorem.paragraph(),
+  //       user_id:""
+  //     }
+  //   })
+  // }) 
 
-  console.log('Seeded database with test user:', todo)
+  // console.log('Seeded database with test user:', todo)
 }
 
 main()
